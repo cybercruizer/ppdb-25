00d767701d4f085db154d24baaa835b8a40a7d72
A Nurkholis <cybercruizer@gmail.com>
2024-11-18 10:33:06 +0700

    gkd

--- resources/views/tesfisik_index.blade.php
+++ resources/views/tesfisik_index.blade.php
@@ -45,7 +45,7 @@
                                 <tr>
                                     <td>{{ $siswa->firstItem() + $key }}</td>
                                     <td>{{ $s->siswa->no_pendaf ?? '-' }}</td>
-                                    <td>{{ strtoupper($s->siswa->nama) ?? '-' }}</td>
+                                    <td>{{ strtoupper($s->siswa->nama ?? 'kosong')}}</td>
                                     <td>{{ $s->guru->nama ?? '-' }}</td>
                                     <td>{{ \Carbon\Carbon::parse($s->created_at)->format('d/m/Y H:i:s') }}</td>
                                     <td>
